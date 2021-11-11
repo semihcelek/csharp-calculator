4c02312558ebb9cda76e3dc688b344038525aec3
Semih Celek <cybermoon1999@gmail.com>
2021-11-11 18:53:17 +0300

    Add UserInput Class for getting basic keyboard inputs.

---
+++ Calculator/UserInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Calculator
+{
+    public class UserInput
+    {
+        private string numInput = "";
+
+        public UserInput()
+        {
+
+        }
+
+        public double getUserInput()
+        {
+            string input = Console.ReadLine();
+            double cleanNum = 0;
+            while (!double.TryParse(input, out cleanNum))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+                input = Console.ReadLine();
+            }
+            return cleanNum;
+        }
+
+        public string getOperatorInput()
+        {
+            return Console.ReadLine();
+
+        }
+
+    }
+}
